06e20aa7cc22d9acedc6fd76afb33eb6cd394ede
Sai Aditya Urumu <saiadityaurumu53@gmail.com>
2024-07-28 23:03:48 -0500

    Completed the Home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen h-screen overflow-hidden flex flex-col">{children}</body>
     </html>
     </ClerkProvider>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,122 @@
-import Image from "next/image";
+
+import {
+  BrainCogIcon, 
+  EyeIcon,
+  GlobeIcon,
+  MonitorSmartphoneIcon,
+  ServerCogIcon,
+  ZapIcon
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from 'next/image';
+
+const features = [
+  {
+    name: "Store your PDF Documents",
+    description: "Keep all your PDF files securely stored and easily accessible anytime, anywhere",
+    icon: GlobeIcon
+  },
+  {
+    name: "Blazing fast Responses",
+    description: "Experience Lightning-fast answers to your queries, ensuring you get the information you need instantly.",
+    icon: ZapIcon
+  },
+  {
+    name: "Chat Memorization",
+    description: "Our intelligent chatbot remembers previous interactions, providing seemless and personalized experience.",
+    icon: BrainCogIcon
+  },
+  {
+    name: "Interactive PDF Viewer",
+    description : "Engage with your PDFs like never before using our intuitive and interactive viewer",
+    icon: EyeIcon
+  },
+  {
+    name : "Cloud Backup",
+    description : "Rest assured knowing your documents are safely backed up on the cloud, protected from the loss or damage",
+    icon : ServerCogIcon
+  },
+  {
+    name: "Responsive Accross devices",
+    description : "Access and chat with your PDFs seemlessly on any device, weather it's your desktop, tablet, or smartphone",
+    icon: MonitorSmartphoneIcon
+  }
+]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Let's Build a SaaS AI application</h1>
+    <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to-indigo-400">
+      <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
+          <div className="flex flex-col justify-center items-center mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl sm:text-center">
+                  <h2  className="text-base font-semibold leading-7 text-indigo-700">
+                    Your interactive document companion
+                  </h2>
+
+                  <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                    Transform your PDFs into Interactive Conversations 
+                  </p>
+                  <p className="mt-6 text-lg leading-8 text-gray-600">
+                    Introducing{" "}
+                    <span className="font-bold text-indigo-600">Chat with PDF.</span>
+                    <br/>
+                    <br/>Upload your document, and our chatbot will answer 
+                    questions, summarize content, and answer all your Qs. Ideally for 
+                    everyone, <span className=" text-indigo-600">
+                    Chat with PDF
+                    </span> {" "}
+                    turns static documents into{" "}
+                    <span className="font-bold">dynamic conversations</span>,
+                    enhancing productivity 10x fold effortlessly
+                  </p>
+              </div>
+
+              <Button asChild className="mt-10">
+                <Link href='/dashboard'>Get Started</Link>
+              </Button>
+          </div>
+
+          <div className="relative overflow-hidden pt-16">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <Image
+                alt="App screenshot"
+                src="https://i.imgur.com/VciRSTI.jpeg"
+                width={2432}
+                height={1442}
+                className="mb-[-0%] rounded-xl shadow-2xl ring-1
+                ring-gray-900/10"
+              />
+              <div aria-hidden="true" className="relative">
+                <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
+              </div>
+            </div>
+
+          </div>
+
+          <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+            <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+              {
+                features.map(feature => (
+                  <div className="relative pl-9">
+                    <dt className="inline font-semibold text-gray-900">
+                        <feature.icon
+                          aria-hidden="true"
+                          className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                        />
+
+                    </dt>
+                    <dd>
+                      {feature.description}
+                    </dd>
+                  </div>
+                ))
+              }
+            </dl>
+          </div>
+
+
+      </div>
     </main>
   );
 }
